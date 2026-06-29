7ebcf560c01f3381afa26ea53b499bbbd5a0915c
Babar Ali <babarali36912@gmail.com>
2026-06-29 15:20:45 +0500

    custom api response and error handling

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -3,10 +3,20 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-       const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
-       console.log(` \n MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`);
+       const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`, {
+        serverSelectionTimeoutMS: 4000
+       });
+       console.log(` \n MongoDB connected successfully! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MONGODB connection Failed ", error);
+        console.error("\n 🔥 === DATABASE CRITICAL ERROR ===");
+        if (error.name==="MongoServerSelectionError") {
+            console.error("Error Type: Cloud Database Unreachable");
+            console.error("Action Required: Please check your Internet Connection or MongoDB Atlas IP Whitelisting (0.0.0.0/0)!");
+        }
+        else {
+            console.error(`Unexpected DB Failure: ${error.message}`);
+        }
+        console.error("===================================\n");
         process.exit(1)
     }
 }
